998ef2e8e4023fce152fdcbed04ec0df304a27e5
horsicq <horsicq@gmail.com>
2021-11-08 18:45:03 +0100

    Fix: 2021-11-08

--- xmime.cmake
+++ xmime.cmake
@@ -1,2 +1,8 @@
-# TODO guard
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+include(${CMAKE_CURRENT_LIST_DIR}/../StaticScan/staticscan.cmake)
+
+set(XMIME_SOURCES
+    ${STATICSCAN_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/xmime.cpp
+)
